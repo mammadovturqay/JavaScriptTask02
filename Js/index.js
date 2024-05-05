ad90f1a415dc453dced0a13b5439e71cf3aee137
= <=Turqay@mail.ru>
2024-05-05 21:07:08 +0400

    first commit

---
+++ Js/index.js
@@ -0,0 +1,111 @@
+console.log("Turqayin Taski ");
+
+// Task1
+// function greetUser() {
+//   const name = prompt("Enter Name ");
+//   console.log("Salam, " + name + "!");
+// }
+
+// greetUser();
+
+//Task 2
+
+// function calculateAge() {
+//   const birthYear = prompt("Enter Birthday :  ");
+//   const currentYear = new Date().getFullYear();
+//   const age = currentYear - parseInt(birthYear);
+//   console.log("You are age !  " + age);
+// }
+
+// calculateAge();
+
+// task3
+// function calculateSquarePerimeter() {
+//   const side = prompt("kvadratin bir terefinin  uzunluğunu daxil edin: ");
+//   const perimeter = 4 * parseInt(side);
+//   console.log("Kvadratın perimetri: " + perimeter);
+// }
+
+// calculateSquarePerimeter();
+
+// task4.
+// function calculateCircleArea() {
+//   const radius = prompt("Dairenin radiusunu daxil edin: ");
+//   const area = Math.PI * Math.pow(parseInt(radius), 2);
+//   console.log("Dairenin  sahesi : " + area);
+// }
+
+// calculateCircleArea();
+
+// task 5
+// function calculateRequiredSpeed() {
+//   const distance = prompt("İki seher arasindaki mesafeni  (km) daxil edin: ");
+//   const hour = prompt("Çatmaq istediyiniz saati  daxil edin: ");
+//   const speed = parseInt(distance) / parseInt(hour);
+//   console.log("Teleb olunan Suret  " + speed + " km/saat");
+// }
+
+// calculateRequiredSpeed();
+
+// task 6
+
+// const exchangeRate = 0.82;
+
+// function convertCurrency() {
+//   const dollars = prompt("Dollar miqdarini  daxil edin: ");
+//   const euros = parseFloat(dollars) * exchangeRate;
+//   console.log("Elde olunan miqdar  " + euros.toFixed(2));
+// }
+
+// convertCurrency();
+
+// task 7
+// function calculateFilesCount() {
+//     const usbSizeGB = prompt("Flash surucunun  hecmini  GB cinsinde  daxil edin: ");
+//     const fileSizeMB = 820; // MB
+//     const filesCount = Math.floor(usbSizeGB * 1024 / fileSizeMB);
+//     console.log("Flash surucusune sigacaq miqdar  " + filesCount);
+//   }
+
+//   calculateFilesCount();
+
+// task 8
+// function calculateChocolate() {
+//   const walletMoney = prompt("Elde olan pulu daxil edin  ");
+//   const chocolatePrice = prompt("Sokoladin qiymeti ? : ");
+//   const chocolateCount = Math.floor(
+//     parseInt(walletMoney) / parseInt(chocolatePrice)
+//   );
+//   const remainingMoney = parseInt(walletMoney) % parseInt(chocolatePrice);
+//   console.log("Ala bileceyiniz sokalad sayisi . !  " + chocolateCount);
+//   console.log("Qalan pul ...  " + remainingMoney);
+// }
+
+// calculateChocolate();
+
+// task9
+
+// function reverseNumber() {
+//   const number = prompt("Uc reqemli eded daxil edin  ");
+//   const digit1 = Math.floor(number / 100);
+//   const digit2 = Math.floor((number % 100) / 10);
+//   const digit3 = number % 10;
+//   const reversedNumber = digit3 * 100 + digit2 * 10 + digit1;
+//   console.log("Daxil etdiyiniz ededin tersi " + reversedNumber);
+// }
+
+// reverseNumber();
+
+// Task 10
+// finish
+
+// function checkEvenNumber() {
+//   const num = prompt("Bir tam eded daxil edin :  ");
+//   if (parseInt(num) % 2 === 0) {
+//     console.log("Daxil etdiyiniz eded cutdur : ");
+//   } else {
+//     console.log("Daxil etdiyiniz eded tekdir ");
+//   }
+// }
+
+// checkEvenNumber();
